5ef96d392ba189afe50f1b065a0d8017ac01b3b3
damar.glh__ <114411272+damar-glh@users.noreply.github.com>
2023-03-12 19:00:43 +0700

    Update Console.js

--- Console.js
+++ Console.js
@@ -2,3 +2,6 @@ let maximum = parseInt(prompt('Masukan nilai maksimal!'));
 while(!maximum){
     maximum = parseInt(prompt('Masukan nilai maksimal!'));
 }
+
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+console.log(targetNum);
